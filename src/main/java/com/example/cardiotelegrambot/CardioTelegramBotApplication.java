a31676fe3206491323482f8f9fcc361bebd6b764
Maxim Al Dandan <almaxim07082005@gmail.com>
2024-04-12 10:08:17 +0300

    Project init

---
+++ src/main/java/com/example/cardiotelegrambot/CardioTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardiotelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardioTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardioTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cardiotelegrambot/CardioTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardiotelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardioTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
